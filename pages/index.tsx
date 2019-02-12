33bd973706369b300ce1cbaf8d3adf2a3d734f4d
Gustavo Ordaz <ordazsgustavo@gmail.com>
2019-02-11 22:30:59 -0500

    setup apollo client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <Mutation
+        mutation={gql`
+          mutation {
+            login(email: "test@test.com", password: "asdasd") {
+              id
+              fullName
+              lastName
+              fullName
+              email
+            }
+          }
+        `}
+      >
+        {mutate => (
+          <button
+            onClick={async () => {
+              const response = await mutate()
+              console.log(response)
+            }}
+          >
+            Test mutation
+          </button>
+        )}
+      </Mutation>
     </Layout>
   )
 }
 
-export default IndexPage;
+export default IndexPage
